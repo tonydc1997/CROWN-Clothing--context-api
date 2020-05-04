c5756554295341296c118c907f2c930c9d8686fc
Anthony Curtis <tonydc1997@gmail.com>
2020-05-04 13:29:24 -0400

    style/fix: Ensure header is full width
    - Remove app-wide left/right padding.
      - Assign padding to :
        - Home page
        - Shop page
        - Header component
    
    - Create new shop.style file for shop Page

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -3,9 +3,10 @@ import { Route } from 'react-router-dom';
 
 import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
 import CollectionPage from '../collection/collection.component';
+import './shop.style.scss';
 
 const ShopPage = ({ match }) => (
-  <div className='shop-page'>
+  <div className="shop-page">
     <Route exact path={`${match.path}`} component={CollectionsOverview} />
     <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
   </div>
