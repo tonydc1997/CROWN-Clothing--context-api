fc7867e46eed4bb811bd4bb7af16f7c407898e8b
Anthony Curtis <tonydc1997@gmail.com>
2020-05-04 16:10:10 -0400

    feat: Ensure Collection preview headers have routes
    - Pass CollectionPreview apropos props.
    
    - Use them in onClick handler.

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -5,9 +5,14 @@ import CollectionItem from '../collection-item/collection-item.component';
 
 import './collection-preview.styles.scss';
 
-const CollectionPreview = ({ title, items }) => (
+const CollectionPreview = ({ title, items, history, match, routeName }) => (
   <div className="collection-preview">
-    <h1 className="title">{title.toUpperCase()}</h1>
+    <h1
+      className="title"
+      onClick={() => history.push(`${match.path}/${routeName}`)}
+    >
+      {title.toUpperCase()}
+    </h1>
     <div className="preview">
       {items
         .filter((item, idx) => idx < 4)
