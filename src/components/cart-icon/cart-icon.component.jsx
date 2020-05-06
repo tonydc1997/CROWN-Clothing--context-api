df7d0c539d5b2c4ec68eb4f196cb12d1467d254f
Anthony Curtis <tonydc1997@gmail.com>
2020-05-06 11:47:10 -0400

    feeat: Pull state from Context.Provider

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -11,6 +11,7 @@ import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 import './cart-icon.styles.scss';
 
 const CartIcon = ({ toggleCartHidden, itemCount }) => {
+  const { toggleHidden } = useContext(CartContext);
   return (
     <div className="cart-icon" onClick={toggleCartHidden}>
       <ShoppingIcon className="shopping-icon" />
