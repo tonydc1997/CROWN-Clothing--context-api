92aae8b842191c5a612a00f42b7c8e9a04566d52
Anthony Curtis <tonydc1997@gmail.com>
2020-05-06 11:48:13 -0400

    chore: Remove redux import & bindings

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -2,7 +2,6 @@ import React, { useContext } from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
-import { toggleCartHidden } from '../../redux/cart/cart.actions';
 import { selectCartItemsCount } from '../../redux/cart/cart.selectors';
 import CartContext from '../../contexts/cart/cart.context';
 
@@ -10,7 +9,7 @@ import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 
 import './cart-icon.styles.scss';
 
-const CartIcon = ({ toggleCartHidden, itemCount }) => {
+const CartIcon = ({ itemCount }) => {
   const { toggleHidden } = useContext(CartContext);
   return (
     <div className="cart-icon" onClick={toggleHidden}>
@@ -20,12 +19,8 @@ const CartIcon = ({ toggleCartHidden, itemCount }) => {
   );
 };
 
-const mapDispatchToProps = dispatch => ({
-  toggleCartHidden: () => dispatch(toggleCartHidden()),
-});
-
 const mapStateToProps = createStructuredSelector({
   itemCount: selectCartItemsCount,
 });
 
-export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
+export default connect(mapStateToProps)(CartIcon);
