a1c5688b09b3f86d3a31cd71341dbfbc1e91024f
Anthony Curtis <tonydc1997@gmail.com>
2020-05-06 11:13:54 -0400

    chore: Import useContext method to Header

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
