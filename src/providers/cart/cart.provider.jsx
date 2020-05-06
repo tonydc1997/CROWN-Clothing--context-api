5ef3c45e8fd9ecd2e710b0f3aca3fe42b9f884b5
Anthony Curtis <tonydc1997@gmail.com>
2020-05-06 12:06:22 -0400

    feat: Create wholesome CartContext functionality

--- src/providers/cart/cart.provider.jsx
+++ src/providers/cart/cart.provider.jsx
@@ -1,2 +1,12 @@
 import React, { createContext, useState, useEffect } from 'react';
 import { removeItemFromCart, addItemToCart } from './cart.utils';
+
+const CartContext = createContext({
+  hidden: true,
+  toggleHidden: () => {},
+  cartItems: [],
+  addItem: () => {},
+  removeItem: () => {},
+  clearItemFromCart: () => {},
+  cartItemsCount: 0,
+});
