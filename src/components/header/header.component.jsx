d2353bbd18a871d2225d4b8fd0d13197d50712d6
Anthony Curtis <tonydc1997@gmail.com>
2020-05-06 11:33:41 -0400

    chore: Import useState hook to Header

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
