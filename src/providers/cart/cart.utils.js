f39f952712eb6d1882e75c3e474226f673643afc
Anthony Curtis <tonydc1997@gmail.com>
2020-05-07 12:06:20 -0400

    feat: Copy/paste redux selector to util fn

--- src/providers/cart/cart.utils.js
+++ src/providers/cart/cart.utils.js
@@ -33,3 +33,9 @@ export const removeItemFromCart = (cartItems, cartItemToRemove) => {
 export const filterItemFromCart = (cartItems, item) => {
   cartItems.filter(cartItem => cartItem.id !== item.id);
 };
+
+export const getCartItemsCount = cartItems =>
+  cartItems.reduce(
+    (accumalatedQuantity, cartItem) => accumalatedQuantity + cartItem.quantity,
+    0
+  );
