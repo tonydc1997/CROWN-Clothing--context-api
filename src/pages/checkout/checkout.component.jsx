63c8329acca3abde9ec5b50a3c46d62ca2ec0c6a
Anthony Curtis <tonydc1997@gmail.com>
2020-05-07 11:44:47 -0400

    feat: Use explicit return to correctly pull state

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -18,33 +18,35 @@ import {
 
 import './checkout.styles.scss';
 
-const CheckoutPage = ({ cartItems, total }) => (
-  <div className="checkout-page">
-    {cartItems.map(cartItem => (
-      <CheckoutItem key={cartItem.id} cartItem={cartItem} />
-    ))}
-    <div className="total">
-      Your total is
-      <span className="totalPrice"> ${total}</span>
+const CheckoutPage = ({ cartItems, total }) => {
+  return (
+    <div className="checkout-page">
+      {cartItems.map(cartItem => (
+        <CheckoutItem key={cartItem.id} cartItem={cartItem} />
+      ))}
+      <div className="total">
+        Your total is
+        <span className="totalPrice"> ${total}</span>
+      </div>
+      <div className="test-warning">
+        <p className="test-title">
+          Please use the credit card below when making payments.
+        </p>
+        <span className="test-message">
+          <img src={CreditCard} alt="Credit Card icon" />
+          4242 4242 4242 4242
+          <br />
+          <img src={CardExp} alt="Calender Icon" />
+          01/30
+          <br />
+          <img src={CardCVC} alt="Secure Lock" />
+          123
+        </span>
+      </div>
+      <StripeCheckoutButton price={total} />
     </div>
-    <div className="test-warning">
-      <p className="test-title">
-        Please use the credit card below when making payments.
-      </p>
-      <span className="test-message">
-        <img src={CreditCard} alt="Credit Card icon" />
-        4242 4242 4242 4242
-        <br />
-        <img src={CardExp} alt="Calender Icon" />
-        01/30
-        <br />
-        <img src={CardCVC} alt="Secure Lock" />
-        123
-      </span>
-    </div>
-    <StripeCheckoutButton price={total} />
-  </div>
-);
+  );
+};
 
 const mapStateToProps = createStructuredSelector({
   cartItems: selectCartItems,
