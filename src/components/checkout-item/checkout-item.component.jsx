dceab1689f77bbc7ee4633db683fc514135af9ef
Anthony Curtis <tonydc1997@gmail.com>
2020-05-07 12:33:08 -0400

    chore: Remove redux bindings & imports

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -1,7 +1,4 @@
 import React, { useContext } from 'react';
-import { connect } from 'react-redux';
-
-import { clearItemFromCart } from '../../redux/cart/cart.actions';
 import { CartContext } from '../../providers/cart/cart.provider';
 
 import './checkout-item.styles.scss';
@@ -35,8 +32,4 @@ const CheckoutItem = ({ cartItem }) => {
   );
 };
 
-const mapDispatchToProps = dispatch => ({
-  clearItem: item => dispatch(clearItemFromCart(item)),
-});
-
-export default connect(null, mapDispatchToProps)(CheckoutItem);
+export default CheckoutItem;
